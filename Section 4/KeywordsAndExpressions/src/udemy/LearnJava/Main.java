a8f80803c8aaac6f1c21cf235635df777dc6f9e3
John Nguyen <38600760+gunyen@users.noreply.github.com>
2020-12-22 15:04:57 -0800

    Add files via upload

---
+++ Section 4/KeywordsAndExpressions/src/udemy/LearnJava/Main.java
@@ -0,0 +1,23 @@
+package udemy.LearnJava;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // a mile is equal to 1.609344 kilometers
+        double kilometres = (100 * 1.609344);
+
+        int highscore = 50;
+        if(highscore == 50) {
+            System.out.println("This is an expression");
+        }
+
+        // In the following code that i will type below, write down what parts of the code
+        // are expressions
+        int score = 100; // score = 100
+        if (score > 90) { // score > 90
+            System.out.println("You got the high score!"); // "You got the high score!"
+            score = 0; // score = 0
+        }
+    }
+}
